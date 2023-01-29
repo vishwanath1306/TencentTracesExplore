ccbb576f34ac0ad78157341a01be14b29b132f46
Vishwanath <oathkeeper@mjolnir.mathcs.emory.edu>
2023-01-28 23:13:26 -0500

    Added info

--- README.md
+++
@@ -1,8 +0,0 @@
-# Tencent Block Traces Exploration
-
-## Converting to 4k 
-The current traces are present as offset values. We need to convert them to 4k files to evaluate a multi-tier cache. 
-
-```
-python3 convert_to_4k.py <IP_FOLDER> <OP_FOLDER>
-```
--- data_transformation.py
+++ data_transformation.py
@@ -4,9 +4,15 @@ import math
 from copy import deepcopy
 import logging
 from multiprocessing import Pool
+import time
 
-
-from util import get_array_indices, load_logging_config, get_op_file_name
+from util import ( 
+    get_array_indices, 
+    load_logging_config, 
+    get_op_file_name,  
+    create_cvv_fd,
+    get_split_file_name
+)
 
 
 '''
@@ -38,6 +44,7 @@ def process_block_trace(filename):
     output_string = get_op_file_name(filename, op_folder)
     op_file_fd = open(output_string, "w")
     op_csv = csv.writer(op_file_fd)
+    
 
     for value, data in enumerate(csv_data):
         
@@ -93,8 +100,78 @@ def convert_to_4k(input_folder):
     # for file_ in files_list:
     #     process_block_trace(filename=file_)
         # break
-    with Pool(2) as p:
+    with Pool(10) as p:
         p.map(process_block_trace, files_list)
+
+def write_only_cvv(filename):
+    curr_time = round( time.time() * 1000)
+    ip_file_fd = open(filename, "r")
+    csv_reader = csv.reader(ip_file_fd)
+    curr_file_name = get_split_file_name(filename)
+
+    array_indices = get_array_indices()
+    output_string = get_op_file_name(filename, op_folder)
+    print(output_string)
+    op_file_fd = open(output_string, "w")
+    
+    curr_cvv_set = set()
+
+    for value, data in enumerate(csv_reader):
+        
+        if value % 1000000 == 0:
+            logging.info(f"Completed lines: {value} from {filename}")
+        
+        curr_cvv = data[array_indices['volume_id']]
+        if curr_cvv not in curr_cvv_set:            
+            op_file_fd.write(f"{curr_cvv}\n")
+            curr_cvv_set.add(curr_cvv)
+    
+    elapsed_time = round(time.time() * 1000) - curr_time
+    logging.info(f"Finished file: {filename} in {elapsed_time} seconds")
+
+    
+
+def process_block_trace_cvv_split(filename):
+    curr_time = round(time.time() *1000)
+    file_fd = open(filename, 'r')
+    csv_reader =  csv.reader(file_fd)
+    cvv_fd_dict = dict()
+    curr_file_name = get_split_file_name(filename)
+    print(curr_file_name)
+    array_indices = get_array_indices()
+    for value, data in enumerate(csv_reader):
+        curr_cvv = data[array_indices['volume_id']]
+        print(curr_cvv)
+    #     if value % 1000000 == 0:
+    #         logging.info("Completed file: {} and {} rows".format(filename, value))
+        
+    #     curr_cvv = data[array_indices['volume_id']]
+    #     if curr_cvv not in cvv_fd_dict.keys():
+    #         try: 
+    #             cvv_fd_dict[curr_cvv] = create_cvv_fd(op_folder, curr_cvv, curr_file_name)    
+    #         except FileExistsError as e:
+    #             pass
+
+    #     cvv_fd_dict[curr_cvv].writerow(data)
+    #     if(value == 3000000):
+    #         break
+    # elapsed_time = round(time.time() * 1000) - curr_time
+    # logging.info(f"Finished file: {filename} in {elapsed_time} seconds")
+
+
+def convert_to_cvv(input_folder):
+    load_logging_config()
+    ip_string = "{}/2*".format(input_folder)
+    files_list = glob.glob(ip_string)
+
+    with Pool(10) as p:
+        p.map(write_only_cvv, files_list)
+    # process_block_trace_cvv_split(files_list[0])
+    # write_only_cvv(files_list[0])
+    # with Pool(1) as p:
+    #     p.map(process_block_trace_cvv_split, files_list)
+    # process_block_trace_cvv_split(files_list[0])
+
 
 if __name__ == "__main__":
     import argparse
@@ -102,10 +179,18 @@ if __name__ == "__main__":
 
     argument_parser.add_argument("ip_folder", help="Enter the folder in which you want to read the input files.")
     argument_parser.add_argument("op_folder", help="Enter the output folder for the trace files.")
+    argument_parser.add_argument("operation", help="Enter the operation: 0 for block; 1 for cvv;")
 
     args = argument_parser.parse_args()
 
     ip_folder = args.ip_folder
     op_folder = args.op_folder
+    operation = int(args.operation)
 
-    convert_to_4k(input_folder=ip_folder)
+    if operation == 0:
+        convert_to_4k(input_folder=ip_folder)
+    elif operation == 1:
+        convert_to_cvv(input_folder=ip_folder)
+    else:
+        print("Wrong operation")
+    
